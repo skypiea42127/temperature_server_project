efbbf0e3afe670c8c49d64ce16f54d36aa9fd03c
skypiea42127 <skypiea42127@hotmail.com>
2015-06-21 13:03:26 +0800

    Update index.js

--- index.js
+++ index.js
@@ -22,8 +22,8 @@ app.get('/api/createDataPoint', function(request, response) {
 	}
 
 	var title = request.query.value;
-	var timeMillis = moment();
-	var time = timeMillis.format('MM/DD hh:mm:ss');
+	var timeMillis = moment("GMT+8");
+	var time = timeMillis.format('YYYY/MM/DD hh:mm:ss');
 	var insert = {
 		_id: timeMillis.unix(),
 		value : title,
